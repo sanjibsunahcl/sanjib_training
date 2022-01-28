bf54012f932612b85f1e29688e0eff747c272def
sanjibsunahcl <98376370+sanjibsunahcl@users.noreply.github.com>
2022-01-28 18:32:32 +0530

    Add files via upload

---
+++ promises.js
@@ -0,0 +1,25 @@
+function usePromise() {
+    
+    const makingCoffee = new Promise((resolve, reject) => {
+        
+        console.log('making coffee is started');
+        let makingDone = true; 
+        if(makingDone) {
+            resolve('making is done');
+        }
+        else {
+            reject('making canceled');
+        }
+
+    });
+    console.log(makingCoffee);
+    return makingCoffee;
+}
+
+usePromise()
+    .then(
+        (status) => {
+            console.log(status);
+            console.log('serve the Coffee');
+        })
+    .catch((status) => console.log(status));
